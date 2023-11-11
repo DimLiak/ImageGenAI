ee8c185aff84a95952fdca727f309aa90f1abe0b
DimLiak <63310994+DimLiak@users.noreply.github.com>
2023-11-10 21:44:05 -0600

    Add files via upload

---
+++ InstaScrapper.py
@@ -0,0 +1,96 @@
+import instaloader
+from instaloader import Instaloader, Profile
+import pandas as pd
+import sys
+
+def _comp(a,b):
+  if a == None:
+    if b == None:
+      return None
+    else:
+      return b['name'] # b['slug']
+  else:
+    return a.name # a.slug
+  
+
+def single_prof_scrapper(USER, PASSWORD, PROFILE, NUM_POSTS):
+
+  # Create instance for scrapiing
+  L = Instaloader()
+
+  # Optionally, login or load session
+  # L.login(USER, PASSWORD) # (login)
+
+  # Define the instagram profile to be scraped
+  profile = Profile.from_username(L.context, PROFILE)
+
+  # Basic profile information
+  username = profile.username
+  followers = profile.followers
+  followees = profile.followees
+  is_business = profile.is_business_account
+  bus_cat = profile.business_category_nameif if (is_business) else "N/A"
+
+  # Start creating our dataset (this is called header as it is common for every folloowing picture)
+  dataset_header = [username, followers, followees, is_business, bus_cat]
+
+  # Sort posts based on the number of likes + comments (could be changed)
+  posts_sorted_by_likes = sorted(profile.get_posts(),
+                                key=lambda p: p.likes + p.comments,
+                                reverse=True)
+
+  # Keep only image posts
+  posts_no_videos = [post for post in posts_sorted_by_likes if post.is_video==False]
+
+  # Profile's Dataset
+  dataset_prof = [dataset_header]*NUM_POSTS
+
+  # Parse all images and extract information
+  for i, post in enumerate(posts_no_videos[:NUM_POSTS]):
+
+    # Extract post information
+    url = post.url
+    caption = post.caption
+    hashtags = post.caption_hashtags
+    likes = post.likes
+    comments = post.comments
+    location = _comp(post.location, post._node['location'])
+    date_utc = post.date_utc
+    img_type = post._node["__typename"]
+
+    # Append post's informations to dataset
+    dataset_prof[i] = dataset_prof[i] + [url, caption, hashtags, likes, comments, location, date_utc, img_type]
+
+  return dataset_prof
+
+def main():
+  if len(sys.argv) != 5:
+      print(f'Usage: {sys.argv[0]} <USER> <PASSWORD> <INSTA_PROFILE> <NUMBER_OF_POSTS>')
+      return -1
+
+  print("Hello..")
+
+  USER = sys.argv[1]
+  PASSWORD = sys.argv[2]
+  PROFILE = sys.argv[3]   # profile to download from
+  NUM_POSTS = int(sys.argv[4]) # number of posts to append to dataset
+
+  data_prof = single_prof_scrapper(USER, PASSWORD, PROFILE, NUM_POSTS)
+
+  headers = ['username', 'followers', 'followees', 'is_business',
+          'business_category', 'post_url', 'caption', 'hashtags',
+          'likes', 'comments', 'location', 'date_utc', 'img_type']
+
+  df = pd.DataFrame(data=data_prof, columns=headers)
+  df.to_csv(f'dataset_{PROFILE}.csv') 
+
+  return 0 
+
+
+if __name__ == "__main__":
+  main()
+
+
+
+
+
